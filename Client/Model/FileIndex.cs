aceaef5341cdd0c8bfcda8db6eba47030cb49cbd
John Nickerson <jnickerson@snowdengroup.com>
2013-10-14 10:42:40 +1000

    Added a constructor to FileIndex to initialise the Files collection it contains.

--- Client/Model/FileIndex.cs
+++ Client/Model/FileIndex.cs
@@ -7,6 +7,11 @@ namespace AssimilationSoftware.MediaSync.Model
 {
     public class FileIndex
     {
+        public FileIndex()
+        {
+            Files = new List<FileHeader>();
+        }
+
         public string MachineName { get; set; }
         public string ProfileName { get; set; }
         public DateTime TimeStamp { get; set; }

ab1a728734620c48e321c3050b98b2fe8cbaeca7
John Nickerson <john.nickerson@ucaqld.com.au>
2014-07-22 09:12:55 +1000

    Converted the unit tests project to reference xUnit.net. Removed old Visual Studio test project files. Changed all projects to compile for .NET 4.5, for consistency.

--- UnitTests/QueuedDiskManagerTest.cs
+++ UnitTests/QueuedDiskManagerTest.cs
@@ -2,19 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NUnit.Framework;
 using AssimilationSoftware.MediaSync.Core;
+using Xunit;
+using AssimilationSoftware.MediaSync.Model;
 
 namespace UnitTests
 {
-    [TestFixture]
     public class QueuedDiskManagerTest
     {
-        [Test]
+        [Fact]
         public void CreateIndex()
         {
-            QueuedDiskCopier q = new QueuedDiskCopier(null, null, null);
+            var mockprofile = new SyncProfile
+            {
+                ProfileName = "mockprofile",
+                ReserveSpace = 5000,
+                SearchPatterns = new List<string>(new string[] { "*.*" }),
+                Participants = new List<ProfileParticipant>(new ProfileParticipant[] {
+                    new ProfileParticipant{
+                        Consumer=true, 
+                        Contributor=true, 
+                        LocalPath=@"C:\temp", 
+                        MachineName=Environment.MachineName, 
+                        SharedPath=@"C:\temp\share"}
+                })
+            };
+            var mockindexer = new Mocks.ConsoleIndexer();
+            QueuedDiskCopier q = new QueuedDiskCopier(mockprofile, mockindexer, "frank");
             var f = q.CreateIndex();
+
+            Assert.Equal(5, f.Files.Count);
         }
     }
 }

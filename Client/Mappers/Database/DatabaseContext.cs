778a57e823d717188cf844a24a0a50ffb8a82c26
John Nickerson <john.nickerson@ucaqld.com.au>
2014-07-25 13:00:07 +1000

    Started migrating database mappers to Entity Framework.

---
+++ Client/Mappers/Database/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using AssimilationSoftware.MediaSync.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssimilationSoftware.MediaSync.Core.Mappers.Database
+{
+    internal class DatabaseContext : DbContext
+    {
+        static DatabaseContext()
+        {
+            Default = new DatabaseContext();
+        }
+
+        public static DatabaseContext Default { get; set; }
+
+        public DbSet<FileIndex> FileIndexes { get; set; }
+        public DbSet<FileHeader> FileHeaders { get; set; }
+        public DbSet<ProfileParticipant> ProfileParticipants { get; set; }
+        public DbSet<SyncProfile> SyncProfiles { get; set; }
+    }
+}
--- Client/Mappers/Database/DbIndexMapper.cs
+++ Client/Mappers/Database/DbIndexMapper.cs
@@ -9,8 +9,9 @@ using System.IO;
 using AssimilationSoftware.MediaSync.Core.Properties;
 using AssimilationSoftware.MediaSync.Model;
 using AssimilationSoftware.MediaSync.Interfaces;
+using AssimilationSoftware.MediaSync.Core.Mappers.Database;
 
-namespace AssimilationSoftware.MediaSync.Mappers.Database
+namespace AssimilationSoftware.MediaSync.Core.Mappers.Database
 {
     /// <summary>
     /// Stores file indexes in a database.
@@ -20,7 +21,6 @@ namespace AssimilationSoftware.MediaSync.Mappers.Database
         private List<string> contents = new List<string>();
         private SyncProfile _options;
         private ProfileParticipant _localSettings;
-        private string _connString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
 
         public DbIndexMapper(SyncProfile options, string machine)
         {
@@ -30,63 +30,34 @@ namespace AssimilationSoftware.MediaSync.Mappers.Database
 
         public void Save(FileIndex index)
         {
-            SqlCeConnection connection = new SqlCeConnection(_connString);
-            SqlCeDataAdapter adapter = new SqlCeDataAdapter("select * from Indexes", connection);
-            adapter.InsertCommand = new SqlCeCommand("Insert Into Indexes (Timestamp, Machine, Profile, RelPath, Size, Hash) Values (@Timestamp, @Machine, @Profile, @RelPath, @Size, @Hash)", connection);
-            adapter.InsertCommand.Parameters.Add("@Timestamp", SqlDbType.DateTime);
-            adapter.InsertCommand.Parameters.Add("@Machine", SqlDbType.NVarChar);
-            adapter.InsertCommand.Parameters.Add("@Profile", SqlDbType.NVarChar);
-            adapter.InsertCommand.Parameters.Add("@RelPath", SqlDbType.NVarChar);
-            adapter.InsertCommand.Parameters.Add("@Size", SqlDbType.BigInt);
-            adapter.InsertCommand.Parameters.Add("@Hash", SqlDbType.NVarChar);
-            // A static timestamp to share among all records.
-            DateTime indextime = DateTime.Now;
-            connection.Open();
-            foreach (FileHeader file in index.Files)
-            {
-                adapter.InsertCommand.Parameters["@Timestamp"] = new SqlCeParameter("@Timestamp", indextime);
-                adapter.InsertCommand.Parameters["@Machine"] = new SqlCeParameter("@Machine", _localSettings.MachineName);
-                adapter.InsertCommand.Parameters["@Profile"] = new SqlCeParameter("@Profile", _options.Name);
-                adapter.InsertCommand.Parameters["@RelPath"] = new SqlCeParameter("@RelPath", Path.Combine(file.RelativePath, file.FileName));
-                adapter.InsertCommand.Parameters["@Size"] = new SqlCeParameter("@Size", file.FileSize);
-                adapter.InsertCommand.Parameters["@Hash"] = new SqlCeParameter("@Hash", file.ContentsHash);
-                adapter.InsertCommand.ExecuteNonQuery();
-            }
-
-            // TODO: Find any indexes older than the last two. Remove them.
-            adapter.DeleteCommand = new SqlCeCommand("Delete From Indexes Where Timestamp Not In (Select Top 2 Timestamp From Indexes Where Machine = @Machine And Profile = @Profile) And Machine = @Machine And Profile = @Profile", connection);
-            adapter.DeleteCommand.Parameters.Add("@Machine", SqlDbType.NVarChar);
-            adapter.DeleteCommand.Parameters.Add("@Profile", SqlDbType.NVarChar);
-            adapter.DeleteCommand.Parameters["@Machine"] = new SqlCeParameter("@Machine", _localSettings.MachineName);
-            adapter.DeleteCommand.Parameters["@Profile"] = new SqlCeParameter("@Profile", _options.Name);
-            adapter.DeleteCommand.ExecuteNonQuery();
-            connection.Close();
+            DatabaseContext.Default.FileIndexes.Add(index);
+            DatabaseContext.Default.SaveChanges();
         }
 
         public FileIndex LoadLatest(string machine, string profile)
         {
-            throw new NotImplementedException();
+            return DatabaseContext.Default.FileIndexes.Where(i => i.MachineName.ToLower() == machine.ToLower() && i.ProfileName.ToLower() == profile.ToLower()).OrderBy(i => i.TimeStamp).LastOrDefault();
         }
 
 
         public List<FileIndex> LoadAll()
         {
-            throw new NotImplementedException();
+            return DatabaseContext.Default.FileIndexes.ToList();
         }
 
         public List<FileIndex> Load(SyncProfile profile)
         {
-            throw new NotImplementedException();
+            return DatabaseContext.Default.FileIndexes.Where(i => i.ProfileName.ToLower() == profile.Name.ToLower()).ToList();
         }
 
         public List<FileIndex> Load(string machine)
         {
-            throw new NotImplementedException();
+            return DatabaseContext.Default.FileIndexes.Where(i => i.MachineName.ToLower() == machine.ToLower()).ToList();
         }
 
         public List<FileIndex> Load(string machine, SyncProfile profile)
         {
-            throw new NotImplementedException();
+            return DatabaseContext.Default.FileIndexes.Where(i => i.MachineName.ToLower() == machine.ToLower() && i.ProfileName.ToLower() == profile.Name.ToLower()).ToList();
         }
     }
 }
